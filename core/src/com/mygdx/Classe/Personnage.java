c03253d60b502fc35492010a4c1477f755851c38
alexisdev87 <alexis-berger@orange.fr>
2015-05-31 10:44:30 +0200

    add more cube

--- core/src/com/mygdx/Classe/Personnage.java
+++ core/src/com/mygdx/Classe/Personnage.java
@@ -1,7 +1,18 @@
 package com.mygdx.Classe;
 
+import com.badlogic.gdx.math.Vector3;
+
+import java.util.ArrayList;
+
 /**
  * Created by Alexis on 29/05/2015.
  */
 public class Personnage {
+
+    protected Vector3 position;
+    protected ArrayList<Skill> skills_tab;
+    protected ArrayList<Object> inventaire;
+
+
+
 }
--- core/src/com/mygdx/game/main.java
+++ core/src/com/mygdx/game/main.java
@@ -1,27 +1,99 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.utils.Array;
 
-public class main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+import java.util.Random;
+
+
+/**
+ * Created by Alexis on 29/05/2015.
+ */
+public class main implements ApplicationListener {
+	public Environment environment;
+	public PerspectiveCamera cam;
+	public CameraInputController camController;
+	public ModelBatch modelBatch;
+	public Model model;
+	public Array<ModelInstance> instance = new Array<ModelInstance>();
+	public ModelInstance blockInstance;
+	public Random jrandom = new Random();
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+		modelBatch = new ModelBatch();
+
+		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.set(10f, 10f, 10f);
+		cam.lookAt(0,0,0);
+		cam.near = 1f;
+		cam.far = 300f;
+		cam.update();
+
+		ModelBuilder modelBuilder = new ModelBuilder();
+		for (float x = -5f; x <= 50f; x+= 5f) {
+			for (float y = -5f; y <= 5f; y+= 5f) {
+				for (float z = -5f; z <= 50f; z+= 5f) {
+
+					model = modelBuilder.createBox(5f, 5f, 5f,
+							new Material(ColorAttribute.createDiffuse(
+									new Color(jrandom.nextFloat(),jrandom.nextFloat(),jrandom.nextFloat(),jrandom.nextFloat()))),
+							Usage.Position | Usage.Normal);
+					blockInstance = new ModelInstance(model);
+					blockInstance.transform.setToTranslation(x,y,z);
+					instance.add(blockInstance);
+				}
+			}
+		}
+		camController = new CameraInputController(cam);
+		Gdx.input.setInputProcessor(camController);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		camController.update();
+
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		modelBatch.begin(cam);
+		modelBatch.render(instance, environment);
+		modelBatch.end();
+	}
+
+	@Override
+	public void dispose() {
+		modelBatch.dispose();
+		model.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
 	}
 }
